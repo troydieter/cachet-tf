88dc912dbfb0f7bcd13237a0a02fd3c4e81bcebd
Troy Dieter <troydieter@gmail.com>
2022-03-14 19:32:25 -0400

    initial

---
+++ vpc.tf
@@ -0,0 +1,40 @@
+# VPC Resources
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "cachet-vpc-${random_id.rando.hex}"
+  cidr = "10.99.0.0/18"
+
+  azs              = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
+
+  create_database_subnet_group = true
+
+  tags = local.common-tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "cachet-sg-rds-${random_id.rando.hex}"
+  description = "SG for RDS ${random_id.rando.hex}"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.common-tags
+}
