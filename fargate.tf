30f640d0fd0b777a03c9d63646ca93478226c52c
Troy Dieter <troydieter@gmail.com>
2022-07-20 20:16:53 -0400

    small version change and fixed subnets

--- fargate.tf
+++ fargate.tf
@@ -20,13 +20,13 @@ module "acm" {
 }
 module "fargate" {
   source              = "cn-terraform/ecs-fargate/aws"
-  version             = "2.0.33"
+  #version             = "2.0.33"
   name_prefix         = var.application
   vpc_id              = module.vpc.vpc_id
   container_image     = "cachethq/docker"
   container_name      = "cachet-${random_id.rando.hex}"
   public_subnets_ids  = module.vpc.public_subnets
-  private_subnets_ids = module.vpc.intra_subnets
+  private_subnets_ids = module.vpc.private_subnets
   lb_internal         = false
   port_mappings = [
     {
@@ -40,6 +40,9 @@ module "fargate" {
       "listener_port" : 443,
       "target_group_port" : 80
     }
+  }
+  map_environment = {
+    "DB_HOST" = module.db.db_instance_address
   }
   default_certificate_arn = module.acm.acm_certificate_arn
 
